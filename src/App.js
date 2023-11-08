8807b2b8afe9091064f402e780a42cd213705a37
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-08 14:40:29 -0600

    Created Footer.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <p>Created by Mariana Tellez Resendiz</p>
+      </footer>
+    </div>
+  );
+}
