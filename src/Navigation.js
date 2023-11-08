9fb2b02048c69944420efb2a80a36e2405acd373
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-08 15:32:58 -0600

    Updated Navigation bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function Navigation() {
   return (
     <div className="Navigation">
-      <p>Navigation</p>
+      <header>
+        <nav>
+          <a href="#section">Home</a>
+          <a href="#section">Try it</a>
+          <a href="#section">About</a>
+          <a href="#section">Questions</a>
+        </nav>
+      </header>
     </div>
   );
 }
