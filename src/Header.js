d78d17aab367e613a8846e2050173d68b398022b
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-09 13:00:39 -0600

    Added styling to the header navigation

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import logo from "./logo.png";
 
 export default function Header() {
   return (
     <div className="Header">
       <header>
+        <img src={logo} alt="IP Detective" title="IP Detective" />
         <nav>
           <a href="#section1">Home</a>
           <a href="#section2">Try it</a>
