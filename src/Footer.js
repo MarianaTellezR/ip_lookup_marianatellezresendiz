8028c3663010d6c3e4f5d0e956131a9013b06c60
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-09 13:35:57 -0600

    Updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,30 @@
 import React from "react";
+import logo from "./logo.png";
 
 export default function Footer() {
   return (
     <div className="Footer">
       <footer>
-        <p>Created by Mariana Tellez Resendiz</p>
+        <section>
+          <img src={logo} alt="IP Detective" title="IP Detective" />
+          <nav>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#try">Try it out</a>
+            <a href="#questions">Questions</a>
+          </nav>
+        </section>
+        <section>
+          <p>
+            Coded and created by{" "}
+            <a
+              href="https://www.linkedin.com/in/mariana-tellez-resendiz/"
+              title="Mariana's LinkedIn"
+            >
+              Mariana Tellez Resendiz
+            </a>
+          </p>
+        </section>
       </footer>
     </div>
   );
