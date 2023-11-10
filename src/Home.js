318e6a98beda6907ca1a3fd084f25f593d5eac2e
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-10 11:37:45 -0600

    Updated Home

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="Home">
-      <p>Home</p>
+      <h1>
+        Explore the power of IP Detective{" "}
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </h1>
+      <p>
+        IP Detective: Uncover IP address details instantly! Enter any IP, and
+        get data on country, region, city, zip, coordinates, timezone, ISP, and
+        view its location on a map. Explore the online world effortlessly.
+      </p>
+      <a href="#try">Give it a try!</a>
     </div>
   );
 }
