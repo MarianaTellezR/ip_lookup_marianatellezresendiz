ced641e1e48ee81cbd4d8815f42a792be4ea0c52
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-15 13:18:42 -0600

    Updated palette colors

--- src/Questions.js
+++ src/Questions.js
@@ -7,7 +7,7 @@ export default function Questions() {
       <h2>Burning Questions</h2>
       <section>
         <div>
-          <h3>Why should I use an IP lookup?</h3>
+          <h3 className="blue">Why should I use an IP lookup?</h3>
           <p>
             IP Lookups offer a valuable location-specific information about IP
             addresses. You can enhance your security, detect cyber threats and
@@ -15,7 +15,7 @@ export default function Questions() {
           </p>
         </div>
         <div>
-          <h3>How precise is the location data?</h3>
+          <h3 className="pink">How precise is the location data?</h3>
           <p>
             Our IP lookup tool delivers an impressive ballpark estimat of the
             exact location. It gets you close to the truth, but there's always
@@ -23,14 +23,14 @@ export default function Questions() {
           </p>
         </div>
         <div>
-          <h3>Do you store my search data?</h3>
+          <h3 className="yellow">Do you store my search data?</h3>
           <p>
             Absolutely not! We value your privacy and don't store or log any
             search queries. Your investigations remain confidential and secure.
           </p>
         </div>
         <div>
-          <h3>Is the IP lookup tool free?</h3>
+          <h3 className="green">Is the IP lookup tool free?</h3>
           <p>
             Yes, our IP lookup tool is entirely free. Enjoy unlimited searches
             and unravel the mysteries of IP addresses without spending a dime.
