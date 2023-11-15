39ea78d956df237d466be3a715057d86383e4602
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-15 12:06:46 -0600

    Created HTML structure for Try.js

--- src/Try.js
+++ src/Try.js
@@ -3,7 +3,22 @@ import React from "react";
 export default function Try() {
   return (
     <div className="Try">
-      <p>Try</p>
+      <h2>IP Detective: Lookup Address</h2>
+      <section>
+        <div>
+          <form>
+            <label for="ip">Enter your IP address:</label>
+            <input
+              type="text"
+              id="ip"
+              name="ip"
+              placeholder="IP Address"
+              required
+            />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
