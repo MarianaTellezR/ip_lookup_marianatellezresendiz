9f18819c3d0564d3bf447d360745bf07661b372e
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-17 17:10:39 -0600

    Added handleFormSubmit and useState

--- src/Try.js
+++ src/Try.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Try.css";
+import Results from "./Results";
 
 export default function Try() {
+  const [componentVisible, setComponentVisible] = useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setComponentVisible(true);
+  };
+
   return (
     <div className="Try">
       <h2>IP Detective: Lookup Address</h2>
       <section>
         <div>
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <label htmlFor="ip">Enter the IP address:</label>
             <input
               type="text"
@@ -20,6 +28,7 @@ export default function Try() {
           </form>
         </div>
       </section>
+      {componentVisible && <Results />}
     </div>
   );
 }
