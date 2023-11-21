99d3bf37d136d0c2e701379304cf35ab55f2ea01
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-21 13:23:50 -0600

    Uploaded images for results

--- src/Result.js
+++ src/Result.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Result.css";
+import {address} from "./address1.png";
+import {ipaddress} from "./address2.png";
+import {location} from "./address3.png";
+import {timezone} from "./address4.png";
 
 export default function Result(props) {
   return (
