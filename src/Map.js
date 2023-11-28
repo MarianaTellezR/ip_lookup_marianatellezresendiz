6ff3069fa102bab11e0276322b83e70e4d231ae3
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-28 11:54:06 -0600

    Added Leaflet map

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Map(props) {
+  const position = [props.lat, props.lon];
   return (
     <div className="Map">
-      <p>{props.lat}, {props.lon}</p>
+      <p>
+        {props.lat}, {props.lon}
+      </p>
+      <MapContainer center={position} zoom={13} style={{ height: "400px" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="OpenStreetMap"
+        />
+        <Marker position={position} />
+      </MapContainer>
     </div>
   );
 }
